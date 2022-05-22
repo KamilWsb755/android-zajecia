2453f1034978fbe54ae111297f2d6eccba92b692
Kamil Trybel <kamil.trybel@gmail.com>
2022-05-22 11:26:30 +0200

    SectionList czyli zagniezdzenie tablic

--- App.js
+++ App.js
@@ -15,18 +15,25 @@ import {
   ScrollView,
   RefreshControl,
   FlatList,
+  SectionList,
 } from 'react-native';
 
 
 const App = () => {
-  const [Items, setItems] = useState([
-    { name: 'Item 1' },
-    { name: 'Item 2' },
-    { name: 'Item 3' },
-    { name: 'Item 4' },
-    { name: 'Item 5' },
-    { name: 'Item 6' },
-  ])
+  const DATA = [
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
+    },
+    {
+      title: 'Title 2',
+      data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
+    },
+    {
+      title: 'Title 3',
+      data: ['Item 3-1', 'Item 3-2', 'Item 3-3'],
+    },
+  ]
   const [Refreshing, setRefreshing] = useState(false);
 
   const onRefresh = () => {
@@ -36,21 +43,17 @@ const App = () => {
   }
 
   return (
-    <FlatList
+    <SectionList
       keyExtractor={(item, index) => index.toString()}
-      data={Items}
+      sections={DATA}
       renderItem={({ item }) => (
+        <Text style={styles.text}>{item}</Text>
+      )}
+      renderSectionHeader={({ section }) => (
         <View style={styles.item}>
-          <Text style={styles.text}>{item.name}</Text>
+          <Text style={styles.text}>{section.title}</Text>
         </View>
       )}
-      refreshControl={
-        <RefreshControl
-          refreshing={Refreshing}
-          onRefresh={onRefresh}
-          colors={['#ff00ff']}
-        />
-      }
     />
   );
 };
