43ba51c612f36f2b46f569b54b938890bc92f72d
Kamil Trybel <kamil.trybel@gmail.com>
2022-05-22 11:18:43 +0200

    Dodawanie elementow za pomoca FlatList

--- App.js
+++ App.js
@@ -14,46 +14,44 @@ import {
   Button,
   ScrollView,
   RefreshControl,
+  FlatList,
 } from 'react-native';
 
 
 const App = () => {
   const [Items, setItems] = useState([
-    { key: 1, item: 'Item 1' },
-    { key: 2, item: 'Item 2' },
-    { key: 3, item: 'Item 3' },
-    { key: 4, item: 'Item 4' },
-    { key: 5, item: 'Item 5' },
+    { name: 'Item 1' },
+    { name: 'Item 2' },
+    { name: 'Item 3' },
+    { name: 'Item 4' },
+    { name: 'Item 5' },
+    { name: 'Item 6' },
   ])
   const [Refreshing, setRefreshing] = useState(false);
 
   const onRefresh = () => {
     setRefreshing(true);
-    setItems([...Items, { key: Items.length + 1, item: `Item ${Items.length + 1}` }])
+    setItems([...Items, { name: `Item ${Items.length + 1}` }])
     setRefreshing(false);
   }
 
   return (
-    <ScrollView
-      style={styles.body}
+    <FlatList
+      keyExtractor={(item, index) => index.toString()}
+      data={Items}
+      renderItem={({ item }) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.name}</Text>
+        </View>
+      )}
       refreshControl={
         <RefreshControl
-          refreshing={false}
+          refreshing={Refreshing}
           onRefresh={onRefresh}
           colors={['#ff00ff']}
         />
       }
-    >
-      {
-        Items.map((object) => {
-          return (
-            <View style={styles.item} key={object.key}>
-              <Text style={styles.text}>{object.item}</Text>
-            </View>
-          )
-        })
-      }
-    </ScrollView >
+    />
   );
 };
 
