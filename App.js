bab02152a07d809f89e4eaa1e4e6d5bd35d397fc
Kamil Trybel <kamil.trybel@gmail.com>
2022-05-22 10:11:32 +0200

    Rozwiazanie zadanie 1 - react-flex

--- App.js
+++ App.js
@@ -18,46 +18,110 @@ const App = () => {
 
   return (
     <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
+
+      {/* Kontener dla 1,2,3 */}
+      <View style={styles.row}>
+        <View style={styles.view1}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={styles.view2}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={styles.view3}>
+          <Text style={styles.text}>3</Text>
+        </View>
       </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}>2</Text>
+
+      {/* Kontener dla 4 */}
+      <View style={styles.row}>
+        <View style={styles.view4}>
+          <Text style={styles.text}>4</Text>
+        </View>
       </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>3</Text>
+
+      {/* Kontener dla 5 */}
+      <View style={styles.row}>
+        <View style={styles.view5}>
+          <Text style={styles.text}>5</Text>
+        </View>
       </View>
+
+
+      {/* View 6 i 7 */}
+      <View style={styles.big_row}>
+        <View style={styles.view6}>
+          <Text style={styles.text}>6</Text>
+        </View>
+        <View style={styles.view7}>
+          <Text style={styles.text}>7</Text>
+        </View>
+      </View>
+
     </View>
+
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    margin: 10,
     flexDirection: 'column',
-    backgroundColor: '#0000ff',
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+  },
+  row: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  big_row: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
   view1: {
-    width: 100,
-    height: 100,
+    flex: 1,
     backgroundColor: '#00ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   view2: {
-    width: 100,
-    height: 100,
+    flex: 2,
     backgroundColor: '#ff00ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   view3: {
-    width: 100,
-    height: 100,
+    flex: 3,
     backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view4: {
+    flex: 1,
+    backgroundColor: '#ff0000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view5: {
+    flex: 1,
+    backgroundColor: '#00ff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view6: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view7: {
+    flex: 1,
+    backgroundColor: '#0000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
